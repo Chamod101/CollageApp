7d339a6a3782f84836f4264de4da9ccf38e61a93
Chamod <chamoddperera99@gmail.com>
2025-02-11 00:18:03 +0530

    Add project files.

---
+++ CollageApp/Controllers/StudentController.cs
@@ -0,0 +1,40 @@
+﻿using CollageApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CollageApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Student>> GetStudents()
+        {
+            return Ok(StudentsRepository.Students);
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<Student> GetStudent(int id)
+        {
+            return Ok(StudentsRepository.Students.Where(n=>n.Id==id).FirstOrDefault());
+        }
+
+        [HttpGet("{name}")]
+        public ActionResult<Student> GetStudentByName(string name)
+        {
+            return Ok(StudentsRepository.Students.Where(n => n.StudentName == name).FirstOrDefault());
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult<bool> DeleteStudent(int id)
+        {
+            if (id < 0) return BadRequest();
+
+            var student = StudentsRepository.Students.Where(n=> n.Id==id).FirstOrDefault();
+            if (student == null) return NotFound();
+
+            StudentsRepository.Students.Remove(student);
+            return Ok(true);
+        }
+    }
+}
---
+++ CollageApp/Models/StudentsRepository.cs
@@ -0,0 +1,29 @@
+﻿namespace CollageApp.Models
+{
+    public static class StudentsRepository
+    {
+        public static List<Student> Students { get; set; } = new List<Student>(){
+                new Student
+                {
+                    Id = 1,
+                    StudentName = "Chamod",
+                    Email ="",
+                    Address =""
+                },
+                new Student
+                {
+                    Id = 2,
+                    StudentName = "Dilushanka",
+                    Email ="",
+                    Address =""
+                },
+                new Student
+                {
+                    Id = 3,
+                    StudentName = "Perera",
+                    Email ="",
+                    Address =""
+                },
+            };
+    }
+}
